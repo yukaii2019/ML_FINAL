d03e233dcc2e6558ac6312ac7f7b8de8f71a467f
yukai <yukai09151702@gmail.com>
2021-01-14 22:42:23 +0800

    GUI.py

--- GUI.py
+++ GUI.py
@@ -2,208 +2,232 @@ import tkinter as tk
 import PIL
 from PIL import Image, ImageDraw
 
-import matplotlib
-import matplotlib.pyplot as plt
 
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-matplotlib.use('TkAgg')
+class GUI():
+    def __init__(self):
+        self.now_text = ''
+        self.real_text = ''
+        self.cursor = ' \N{BLACK DOWN-POINTING TRIANGLE} '
+        self.prediction_map = {
+                                0:'0',
+                                1:'1',
+                                2:'2',
+                                3:'3',
+                                4:'4',
+                                5:'5',
+                                6:'6',
+                                7:'7',
+                                8:'8',
+                                9:'9',
+                                10:'\N{PLUS SIGN}',
+                                11:'\N{HYPHEN-MINUS}',
+                                12:'\N{MULTIPLICATION SIGN}',
+                                13:'\N{DIVISION SIGN}',
+                                14:'\N{EQUALS SIGN}',
+                            }
+        self.cursor_position = 0
+        self.real_cursor_position = 0
 
+    def save(self):
+        self.image1.save('image.jpg')
+        self.cv.delete("all")
+        self.draw.rectangle([(0,0),(28,28)], fill="white")
 
+    def paint(self,e):
+        x, y = e.x, e.y
+        width = self.cv.winfo_width()
+        height = self.cv.winfo_height()
+        x_pillow,y_pillow = x*28/width,y*28/height
 
-def save():
-    image1.save('image.jpg')
-    cv.delete("all")
-    draw.rectangle([(0,0),(28,28)], fill="white")
+        #canvas
+        self.cv.create_oval(x-10,y+10,x+10,y-10,fill = "black")
 
-def paint(e):
-    x, y = e.x, e.y
-    width = cv.winfo_width()
-    height = cv.winfo_height()
-    x_pillow,y_pillow = x*28/width,y*28/height
+        #pil
+        self.draw.ellipse([(x_pillow-1,y_pillow-1),(x_pillow+1,y_pillow+1)], fill="black")
+        self.cv.bind('<B1-Motion>', self.paint)
 
-    #canvas
-    cv.create_oval(x-10,y+10,x+10,y-10,fill = "black")
+    def button_add(self):
+        width = self.cv.winfo_width()
+        height = self.cv.winfo_height()
+        print(width)
+        print(height)
 
-    #pil
-    draw.ellipse([(x_pillow-1,y_pillow-1),(x_pillow+1,y_pillow+1)], fill="black")
-    cv.bind('<B1-Motion>', paint)
+        return
 
-def button_add():
-    width = cv.winfo_width()
-    height = cv.winfo_height()
-   # print(width)
-    #print(height)
+    def button_1(self):
+        self.equation_space =  (self.frm_display_equation.winfo_width()-26)//25
+        self.real_text = self.real_text[:self.real_cursor_position]+self.prediction_map[self.prediction[0]]+self.real_text[self.real_cursor_position:]
+        if self.cursor_position != self.equation_space:
+            self.cursor_position = self.cursor_position + 1
+            self.cursor = '   '+self.cursor
+            self.lbl_3["text"] = self.cursor
+        if self.real_cursor_position != len(self.real_text):
+            self.real_cursor_position = self.real_cursor_position + 1
+        self.lbl_2["text"] = self.real_text[self.real_cursor_position-self.cursor_position:self.real_cursor_position+(self.equation_space-self.cursor_position)+1]
 
-    return
+    def button_2(self):
+        self.equation_space =  (self.frm_display_equation.winfo_width()-26)//25
+        self.real_text = self.real_text[:self.real_cursor_position]+self.prediction_map[self.prediction[1]]+self.real_text[self.real_cursor_position:]
+        if self.cursor_position != self.equation_space:
+            self.cursor_position = self.cursor_position + 1
+            self.cursor = '   '+self.cursor
+            self.lbl_3["text"] = self.cursor
+        if self.real_cursor_position != len(self.real_text):
+            self.real_cursor_position = self.real_cursor_position + 1
+        self.lbl_2["text"] = self.real_text[self.real_cursor_position-self.cursor_position:self.real_cursor_position+(self.equation_space-self.cursor_position)+1]
 
-now_text = '\N{Reverse Solidus}'
-cursor = ' \N{BLACK DOWN-POINTING TRIANGLE} '
+    def button_3(self):
+        self.equation_space =  (self.frm_display_equation.winfo_width()-26)//25
+        self.real_text = self.real_text[:self.real_cursor_position]+self.prediction_map[self.prediction[2]]+self.real_text[self.real_cursor_position:]
+        if self.cursor_position != self.equation_space:
+            self.cursor_position = self.cursor_position + 1
+            self.cursor = '   '+self.cursor
+            self.lbl_3["text"] = self.cursor
+        if self.real_cursor_position != len(self.real_text):
+            self.real_cursor_position = self.real_cursor_position + 1
+        self.lbl_2["text"] = self.real_text[self.real_cursor_position-self.cursor_position:self.real_cursor_position+(self.equation_space-self.cursor_position)+1]
 
-def button_1():
-    global now_text
-    width = lbl_2.winfo_width()
-    now_text = now_text[:-1] + prediction_map[prediction[0]]+'\N{Reverse Solidus}'
-    #print((width-26)//25)
-    if len(now_text)>(width-26)//25:   
-        lbl_2["text"] = now_text[len(now_text)-(width-26)//25:]
-    else:
-        lbl_2["text"] = now_text
-
-def button_2():
-    global now_text
-    width = lbl_2.winfo_width()
-    now_text = now_text[:-1] + prediction_map[prediction[1]]+'\N{Reverse Solidus}'
-    if len(now_text)>(width-26)//25:   
-        lbl_2["text"] = now_text[len(now_text)-(width-26)//25:]
-    else:
-        lbl_2["text"] = now_text
-
-def button_3():
-    global now_text
-    width = lbl_2.winfo_width()
-    now_text = now_text[:-1] + prediction_map[prediction[2]]+'\N{Reverse Solidus}'
-    if len(now_text)>(width-26)//25:   
-        lbl_2["text"] = now_text[len(now_text)-(width-26)//25:]
-    else:
-        lbl_2["text"] = now_text
-
-def button_4():
-    global now_text
-    width = lbl_2.winfo_width()
-    now_text = now_text[:-1] + prediction_map[prediction[3]]+'\N{Reverse Solidus}'
-    if len(now_text)>(width-26)//25:   
-        lbl_2["text"] = now_text[len(now_text)-(width-26)//25:]
-    else:
-        lbl_2["text"] = now_text
-
-def change_equation_bar(e):
-    global now_text
-    width = lbl_2.winfo_width()
-    if len(now_text)>(width-26)//25:   
-        lbl_2["text"] = now_text[len(now_text)-(width-26)//25:]
-    else:
-        lbl_2["text"] = now_text
-
-
-def left():
-    global cursor
-    width = lbl_2.winfo_width()
-
-
-def right():
-
-prediction_map = {
-    0:'0',
-    1:'1',
-    2:'2',
-    3:'3',
-    4:'4',
-    5:'5',
-    6:'6',
-    7:'7',
-    8:'8',
-    9:'9',
-    10:'\N{PLUS SIGN}',
-    11:'\N{HYPHEN-MINUS}',
-    12:'\N{MULTIPLICATION SIGN}',
-    13:'\N{DIVISION SIGN}',
-    14:'\N{EQUALS SIGN}',
-}
+    def button_4(self):
+        self.equation_space =  (self.frm_display_equation.winfo_width()-26)//25
+        self.real_text = self.real_text[:self.real_cursor_position]+self.prediction_map[self.prediction[3]]+self.real_text[self.real_cursor_position:]
+        if self.cursor_position != self.equation_space:
+            self.cursor_position = self.cursor_position + 1
+            self.cursor = '   '+self.cursor
+            self.lbl_3["text"] = self.cursor
+        if self.real_cursor_position != len(self.real_text):
+            self.real_cursor_position = self.real_cursor_position + 1
+        self.lbl_2["text"] = self.real_text[self.real_cursor_position-self.cursor_position:self.real_cursor_position+(self.equation_space-self.cursor_position)+1]
+    def change_equation_bar(self,e):
+        width = self.lbl_2.winfo_width()
+        if len(self.now_text)>(width-26)//25:   
+            self.lbl_2["text"] = self.now_text[len(self.now_text)-(width-26)//25:]
+        else:
+            self.lbl_2["text"] = self.now_text
 
 
-prediction = [0,1,2,14]
-
-image1 = PIL.Image.new('RGB', (28, 28), 'white')
-draw = ImageDraw.Draw(image1)
-
-
-
-
-window = tk.Tk()
-window.title("Calculator")
-
-window.rowconfigure(0,weight = 0,minsize = 25)
-window.rowconfigure(1,weight = 1,minsize = 60)
-window.rowconfigure(2,weight = 1,minsize = 85)
-window.rowconfigure(3,weight = 1,minsize = 310)
-window.columnconfigure(0,weight = 1,minsize = 110)
-window.columnconfigure(1,weight = 1,minsize = 410)
-window.columnconfigure(2,weight = 1,minsize = 110)
-
-window.bind( "<Configure>", change_equation_bar)
-
-frm_right_btn = tk.Frame(master = window, relief = tk.RIDGE, borderwidth = 5)
-frm_right_btn.rowconfigure([0,1,2,3],weight = 1,minsize =75)
-frm_right_btn.columnconfigure(0,weight = 1,minsize = 100)
-
-frm_writing_board = tk.Frame(master = window, relief = tk.RIDGE, borderwidth = 5)
-frm_writing_board.rowconfigure(0,weight = 1,minsize = 300)
-frm_writing_board.columnconfigure(0,weight = 1,minsize = 400)
-
-frm_display = tk.Frame(master = window, relief = tk.RIDGE, borderwidth = 5)
-frm_display.rowconfigure(0,weight = 1,minsize = 75)
-frm_display.columnconfigure(0,weight = 1,minsize = 600)
-
-frm_left_btn = tk.Frame(master = window, relief = tk.RIDGE, borderwidth = 5)
-frm_left_btn.rowconfigure([0,1,2,3],weight = 1,minsize =75)
-frm_left_btn.columnconfigure([0,1],weight = 1,minsize = 50)
-
-frm_display_equation = tk.Frame(master = window, relief = tk.RIDGE, borderwidth = 5)
-frm_display_equation.rowconfigure(0,weight = 1,minsize = 50)
-frm_display_equation.columnconfigure(0,weight = 1,minsize = 600)
-
-frm_cursor = tk.Frame(master = window, relief = tk.RIDGE, borderwidth = 5)
-frm_cursor.rowconfigure(0,weight = 1,minsize = 15)
-frm_cursor.columnconfigure(0,weight = 1,minsize = 600)
-
-frm_right_btn.grid(row = 3, column = 2, sticky="nsew")
-frm_writing_board.grid(row = 3, column = 1, sticky="nsew")
-frm_display.grid(row = 2, columnspan = 3, sticky="nsew")
-frm_left_btn.grid(row = 3, column = 0, sticky="nsew")
-frm_display_equation.grid(row = 1, columnspan = 3, sticky="nsew")
-frm_cursor.grid(row = 0, columnspan = 3, sticky="nsew")
+    def left(self):
+        self.equation_space =  (self.frm_display_equation.winfo_width()-26)//25
+        if self.cursor_position != 0:
+            self.cursor_position = self.cursor_position - 1
+            self.real_cursor_position = self.real_cursor_position - 1
+            self.cursor = self.cursor[3:]
+            self.lbl_3["text"] = self.cursor
+        elif self.real_cursor_position  != 0:      
+            self.real_cursor_position = self.real_cursor_position - 1
+            self.lbl_2["text"] = self.real_text[self.real_cursor_position-self.cursor_position:self.real_cursor_position+(self.equation_space-self.cursor_position)+1]
+            
 
 
-lbl_1 = tk.Label(master = frm_display, bg='white', relief = tk.RIDGE, borderwidth = 1)
-lbl_1.grid(row = 0, column = 0, sticky="nsew")
+    def right(self):
+        self.equation_space =  (self.frm_display_equation.winfo_width()-26)//25
+        if self.cursor_position != self.equation_space:
+            self.cursor_position = self.cursor_position + 1
+            self.real_cursor_position = self.real_cursor_position + 1
+            self.cursor = '   '+self.cursor
+            self.lbl_3["text"] = self.cursor
+        elif self.real_cursor_position != len(self.real_text):
+            self.real_cursor_position = self.real_cursor_position + 1
+            self.lbl_2["text"] = self.real_text[self.real_cursor_position-self.cursor_position:self.real_cursor_position+(self.equation_space-self.cursor_position)+1]
+        
+
+    def main(self, pred):
+        self.prediction = pred
+        
+        self.window = tk.Tk()
+        self.window.title("Calculator")
+
+        self.image1 = PIL.Image.new('RGB', (28, 28), 'white')
+        self.draw = ImageDraw.Draw(self.image1)
+
+        self.window.rowconfigure(0,weight = 0,minsize = 25)
+        self.window.rowconfigure(1,weight = 1,minsize = 60)
+        self.window.rowconfigure(2,weight = 1,minsize = 85)
+        self.window.rowconfigure(3,weight = 1,minsize = 310)
+        self.window.columnconfigure(0,weight = 1,minsize = 110)
+        self.window.columnconfigure(1,weight = 1,minsize = 410)
+        self.window.columnconfigure(2,weight = 1,minsize = 110)
+
+        self.window.bind( "<Configure>", self.change_equation_bar)
+
+        self.frm_right_btn = tk.Frame(master = self.window, relief = tk.RIDGE, borderwidth = 5)
+        self.frm_right_btn.rowconfigure([0,1,2,3],weight = 1,minsize =75)
+        self.frm_right_btn.columnconfigure(0,weight = 1,minsize = 100)
+
+        self.frm_writing_board = tk.Frame(master = self.window, relief = tk.RIDGE, borderwidth = 5)
+        self.frm_writing_board.rowconfigure(0,weight = 1,minsize = 300)
+        self.frm_writing_board.columnconfigure(0,weight = 1,minsize = 400)
+
+        self.frm_display = tk.Frame(master = self.window, relief = tk.RIDGE, borderwidth = 5)
+        self.frm_display.rowconfigure(0,weight = 1,minsize = 75)
+        self.frm_display.columnconfigure(0,weight = 1,minsize = 600)
+
+        self.frm_left_btn = tk.Frame(master =self.window, relief = tk.RIDGE, borderwidth = 5)
+        self.frm_left_btn.rowconfigure([0,1,2,3],weight = 1,minsize =75)
+        self.frm_left_btn.columnconfigure([0,1],weight = 1,minsize = 50)
+
+        self.frm_display_equation = tk.Frame(master = self.window, relief = tk.RIDGE, borderwidth = 5)
+        self.frm_display_equation.rowconfigure(0,weight = 1,minsize = 50)
+        self.frm_display_equation.columnconfigure(0,weight = 1,minsize = 600)
+
+        self.frm_cursor = tk.Frame(master = self.window, relief = tk.RIDGE, borderwidth = 5)
+        self.frm_cursor.rowconfigure(0,weight = 1,minsize = 15)
+        self.frm_cursor.columnconfigure(0,weight = 1,minsize = 600)
+
+        self.frm_right_btn.grid(row = 3, column = 2, sticky="nsew")
+        self.frm_writing_board.grid(row = 3, column = 1, sticky="nsew")
+        self.frm_display.grid(row = 2, columnspan = 3, sticky="nsew")
+        self.frm_left_btn.grid(row = 3, column = 0, sticky="nsew")
+        self.frm_display_equation.grid(row = 1, columnspan = 3, sticky="nsew")
+        self.frm_cursor.grid(row = 0, columnspan = 3, sticky="nsew")
 
 
-lbl_2 = tk.Label(master = frm_display_equation, bg='white', relief = tk.RIDGE, borderwidth = 1,font = ("Courier",30),anchor = 'w',justify=tk.RIGHT)
-lbl_2.grid(row = 0, column = 0, sticky="nsew")
-
-lbl_3 = tk.Label(master = frm_cursor, bg='white', relief = tk.RIDGE, borderwidth = 1,font = ("Courier",10),anchor = 'w',justify=tk.RIGHT)
-lbl_3.grid(row = 0, column = 0, sticky="nsew")
-
-cv = tk.Canvas(master = frm_writing_board, bg='white', relief = tk.RIDGE, borderwidth = 1, width=400, height=300)
-cv.grid(row = 0, column = 0,sticky="nsew")
-cv.bind('<Button-1>', paint)
-
-#right buttons
-btn_1 = tk.Button(master = frm_right_btn, text = '1' ,relief = tk.RIDGE, borderwidth = 1, command = button_1)
-btn_2 = tk.Button(master = frm_right_btn, text = '2' ,relief = tk.RIDGE, borderwidth = 1, command = button_2)
-btn_3 = tk.Button(master = frm_right_btn, text = '3' ,relief = tk.RIDGE, borderwidth = 1, command = button_3)
-btn_4 = tk.Button(master = frm_right_btn, text = '4' ,relief = tk.RIDGE, borderwidth = 1, command = button_4)
-btn_1.grid(row = 0, column = 0,sticky="nsew")
-btn_2.grid(row = 1, column = 0,sticky="nsew")
-btn_3.grid(row = 2, column = 0,sticky="nsew")
-btn_4.grid(row = 3, column = 0,sticky="nsew")
+        self.lbl_1 = tk.Label(master = self.frm_display, bg='white', relief = tk.RIDGE, borderwidth = 1)
+        self.lbl_1.grid(row = 0, column = 0, sticky="nsew")
 
 
-# left buttons
-btn_5 = tk.Button(master = frm_left_btn, text = 'DEL', relief = tk.RIDGE, borderwidth = 1, command = button_add)
-btn_6 = tk.Button(master = frm_left_btn, text = 'AC' , relief = tk.RIDGE, borderwidth = 1 , command = button_add)
-btn_7 = tk.Button(master = frm_left_btn, text = "\N{RIGHTWARDS BLACK ARROW}", relief = tk.RIDGE, borderwidth = 1, command = button_add)
-btn_8 = tk.Button(master = frm_left_btn, text = "\N{LEFTWARDS BLACK ARROW}" , relief = tk.RIDGE, borderwidth = 1, command = left)
-btn_9 = tk.Button(master = frm_left_btn, text = '=', command = save, relief = tk.RIDGE, borderwidth = 1)
+        self.lbl_2 = tk.Label(master = self.frm_display_equation, bg='white', relief = tk.RIDGE, borderwidth = 1,font = ("Courier",30),anchor = 'w',justify=tk.RIGHT)
+        self.lbl_2.grid(row = 0, column = 0, sticky="nsew")
+
+        self.lbl_3 = tk.Label(master = self.frm_cursor, bg='white', relief = tk.RIDGE, borderwidth = 1,font = ("Courier",10),anchor = 'w',justify=tk.RIGHT,text = self.cursor )
+        self.lbl_3.grid(row = 0, column = 0, sticky="nsew")
+
+        self.cv = tk.Canvas(master = self.frm_writing_board, bg='white', relief = tk.RIDGE, borderwidth = 1, width=400, height=300)
+        self.cv.grid(row = 0, column = 0,sticky="nsew")
+        self.cv.bind('<Button-1>', self.paint)
+
+        #right buttons
+        self.btn_1 = tk.Button(master = self.frm_right_btn, text = '1' ,relief = tk.RIDGE, borderwidth = 1, command = self.button_1)
+        self.btn_2 = tk.Button(master = self.frm_right_btn, text = '2' ,relief = tk.RIDGE, borderwidth = 1, command = self.button_2)
+        self.btn_3 = tk.Button(master = self.frm_right_btn, text = '3' ,relief = tk.RIDGE, borderwidth = 1, command = self.button_3)
+        self.btn_4 = tk.Button(master = self.frm_right_btn, text = '4' ,relief = tk.RIDGE, borderwidth = 1, command = self.button_4)
+        self.btn_1.grid(row = 0, column = 0,sticky="nsew")
+        self.btn_2.grid(row = 1, column = 0,sticky="nsew")
+        self.btn_3.grid(row = 2, column = 0,sticky="nsew")
+        self.btn_4.grid(row = 3, column = 0,sticky="nsew")
 
 
-btn_5.grid(row = 0, columnspan = 2,sticky="nsew")
-btn_6.grid(row = 1, columnspan = 2,sticky="nsew")
-btn_7.grid(row = 2, column = 1,sticky="nsew")
-btn_8.grid(row = 2, column = 0,sticky="nsew")
-btn_9.grid(row = 3, columnspan = 2,sticky="nsew")
+        # left buttons
+        self.btn_5 = tk.Button(master = self.frm_left_btn, text = 'DEL', relief = tk.RIDGE, borderwidth = 1, command = self.button_add)
+        self.btn_6 = tk.Button(master = self.frm_left_btn, text = 'AC' , relief = tk.RIDGE, borderwidth = 1 , command = self.button_add)
+        self.btn_7 = tk.Button(master = self.frm_left_btn, text = "\N{RIGHTWARDS BLACK ARROW}", relief = tk.RIDGE, borderwidth = 1, command = self.right)
+        self.btn_8 = tk.Button(master = self.frm_left_btn, text = "\N{LEFTWARDS BLACK ARROW}" , relief = tk.RIDGE, borderwidth = 1, command = self.left)
+        self.btn_9 = tk.Button(master = self.frm_left_btn, text = '=', command = self.save, relief = tk.RIDGE, borderwidth = 1)
+
+
+        self.btn_5.grid(row = 0, columnspan = 2,sticky="nsew")
+        self.btn_6.grid(row = 1, columnspan = 2,sticky="nsew")
+        self.btn_7.grid(row = 2, column = 1,sticky="nsew")
+        self.btn_8.grid(row = 2, column = 0,sticky="nsew")
+        self.btn_9.grid(row = 3, columnspan = 2,sticky="nsew")
+
+        
+        
+        
+        self.window.mainloop()
+
+gui = GUI()
+prediction = [0,1,2,14] 
+gui.main(prediction)
 
 
 
-
-window.mainloop()
